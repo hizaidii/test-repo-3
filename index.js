6365273de0864f1f1b8dce06a8873de782aa4b08
hizaidii <90659514+hizaidii@users.noreply.github.com>
2023-02-06 23:36:22 -0500

    started with js

---
+++ index.js
@@ -0,0 +1,31 @@
+// First program ever! - Show Hello Geeks on console 
+
+console.log('Hello Geeks!!');
+
+console.log("hello")
+
+console.log(2+4);
+
+// Variables!!
+
+// VAR type variable
+var herName;
+
+herName = "Samar";
+
+console.log(herName)
+
+// LET type variable
+
+let text; //creating or declaring a variable
+
+text = "I'm sleepy...NOT" //assigning a value
+
+console.log(text)
+
+// CONST variable
+
+const number = 3;
+
+console.log(number)
+
